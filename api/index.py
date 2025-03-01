a4214bd17ab2631a377aaaf0dc33b489ce93238d
Emil Rustamli <98512663+EmilRustamli@users.noreply.github.com>
2025-03-01 01:13:17 -0800

    V7.4.1

--- api/index.py
+++ api/index.py
@@ -1,5 +1,17 @@
+from flask import Flask
+import sys
+import os
+
+# Add the parent directory to sys.path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Import the Flask app from app.py
 from app import app
 
-# This is the handler function Vercel will call
-def handler(request, context):
-    return app(request, context) 
+# This is the handler that Vercel will use
+# Do not modify this function name or signature
+def handler(request):
+    """
+    Vercel serverless function handler that exposes the Flask app
+    """
+    return app 
